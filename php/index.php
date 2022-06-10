f73a118d4f8104780d93515a609e521602b89399
alexcapoluongo <alexcapoluongo@icloud.com>
2022-06-10 14:35:38 +0200

    php logic milestone 1 aded

--- php/index.php
+++ php/index.php
@@ -6,19 +6,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Milestone 1</title>
     <!-- css custom -->
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
     <?php include "../database.php"?>
-    <div class="card">
+    
         <ul>
             <?php foreach($database as $album) {
-                echo '<li>' . '<img src=' . $album['poster'] . '>' . '</li>';          
+                echo '<div class="card">' . '<li>' . '<img src=' . $album['poster'] . '>' . '</li>';          
                 echo '<li>' . $album['title'] . '</li>';
                 echo '<li>' . $album['author'] . '</li>';
-                echo '<li>' . $album['year'] . '</li><br>';
+                echo '<li>' . $album['year'] . '</li>' . '</div>' . '<br>';
             }
             ?>
         </ul>
-    </div>
+
 </body>
 </html>
