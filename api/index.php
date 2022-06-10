df3e86ec4a1e8d3ca702c92de19d6f22d8745738
alexcapoluongo <alexcapoluongo@icloud.com>
2022-06-10 15:00:50 +0200

    server.php added and scaffolding index json

---
+++ api/index.php
@@ -0,0 +1,19 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Milestone 2</title>
+    <!-- css custom -->
+    <link rel="stylesheet" href="../css/style.css">
+    <!-- vuejs -->
+    <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
+</head>
+<body>
+    
+
+<!-- script -->
+<script src="js/style.js"></script>
+</html>
